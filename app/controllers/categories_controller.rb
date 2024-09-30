fb52d71047b82d93fae0adca73a5906164ca803b
mathew newton <mattynewts@gmail.com>
2024-09-30 22:50:31 +0800

    added catergory routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,23 +4,32 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @categories = Category.find(params[:id])
-    @categories = Category.all
+    @category = Category.find(params[:id])
   end
 
   def new
-    @categories = Category.new()
-    @categories = Category.find(category_param[:name])
-    @categories.save
+    @categories = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to category_path(@category), notice: 'Category was successfully created.'
+    else
+      render :new, status: :unproccessable_entity
+    end
   end
 
   def destroy
-
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, status: :see_other
   end
 
   private
 
   def category_params
-
+    params.require(:category).permit(:name)
   end
 end
