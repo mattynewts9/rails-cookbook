d92a68f1075b3672296ce277380a5948253c93e8
mathew newton <mattynewts@gmail.com>
2024-10-02 22:41:49 +0800

    update db:seed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks, dependent: :destroy
   has_many :recipes, through: :bookmarks
   validates :name, presence: true, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -8,8 +11,22 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+# url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{@category}"
+# category_serialized = URI.parse(url).read
+# @category = JSON.parse(category_serialized)
+
+# puts "#{@category.name["name"]} - #{@category.description["description"]}"
+
+url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{@category.name}"
+category_serialized = URI.parse(url).read
+category = JSON.parse(category_serialized)
+
+puts "#{category.name["name"]} - #{category.description["description"]}"
+
 # db/seeds.rb
 Recipe.destroy_all
+
+
 
 Category.create(name: 'Dessert')
 Category.create(name: 'Vegeterian')
@@ -22,30 +39,30 @@ Recipe.create!([
     name: "Spaghetti Carbonara",
     description: "A classic Italian pasta dish made with eggs, cheese, pancetta, and pepper.",
     rating: 9.5,
-    image_url: ""
+    image_url: "https://www.usdairy.com/getmedia/1a9efa87-5f85-4392-8a67-3836755be069/creamy_carbonara.jpg?width=1500&height=1001&ext=.jpg"
   },
   {
     name: "Chicken Tikka Masala",
     description: "A popular Indian dish with grilled marinated chicken simmered in a creamy tomato curry sauce.",
     rating: 8.8,
-    image_url: "",
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYo6i5Nif8pf7FCNWjwhnkG_CI1VXYLDaBUA&s",
   },
   {
     name: "Beef Stroganoff",
     description: "A Russian comfort dish of sautéed beef in a rich, creamy mushroom sauce served over noodles.",
     rating: 8.2,
-    image_url: ""
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSurr1btAiTq264orhOzH64TnhxzWS3VFPrsA&s"
   },
   {
     name: "Vegetarian Chili",
     description: "A hearty and spicy chili made with a variety of beans, tomatoes, and vegetables.",
     rating: 7.9,
-    image_url: ""
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2KSlBnFzey4VDaKplO7FdD7x2CCg_3lO6tg&s"
   },
   {
     name: "Chocolate Lava Cake",
     description: "A decadent chocolate dessert with a molten center that flows when you cut into it.",
     rating: 9.7,
-    image_url: ""
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYg-bd41gp07DGUmjqX24KYf9f6GMce3OEw&s"
   }
 ])
